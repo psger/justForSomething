c816e46ae09358c473f36e6ff964cb2c49805ecb
passenger <passengerlcw@gmail.com>
2018-03-13 19:16:56 +0800

    resolve some bug

--- 004.cpp
+++ 004.cpp
@@ -2,42 +2,55 @@
 
 using namespace std;
 
-//#define __tmain main;
+#define __tmain main
+
 
 class Solution
 {
 public:
-    void replaceSpace(char *str,int length)
+	void replaceSpace(char *str,int length)
     {
-      int i,j;
-      int count = 0;
-      int newLenhth = length;
-      for(i=0;i<length;i++);{
-          if(str[i] == ' ')
-          count++;
-      }
-      newLenhth = length + count*2;
-
-      for(i=length-1,j=newLenhth-1;i>=0 && j>=0;){
-        if(str[i] == ' '){
-          str[j--] = '0';
-          str[j--] = '2';
-          str[j--] = '%';
-          i--;
+        int i, j;
+        int count = 0;
+        int len = length;
+        for(int i = 0; i < length; i++)
+        {
+            if(str[i] == ' ')
+            {
+                count++;
+            }
         }
-        else
-        str[j--] = str[i--];
-      }
-      str[newLenhth] = '0';
-    }
+
+        len = length + count * 2;
+        for(i = length - 1, j = len - 1;
+            i >= 0 && j >= 0;)
+        {
+            if(str[i] == ' ')
+            {
+                str[j--] = '0';
+                str[j--] = '2';
+                str[j--] = '%';
+                i--;
+            }
+            else
+            {
+                str[j--]  = str[i--];
+            }
+        }
+        str[len] = '0';
+
+	}
 };
 
 
-int main( )
+int __tmain( )
 {
-  char str[10+1] = "a b c d";
-  Solution solu;
-  solu.replaceSpace(str,10);
-  cout << str <<endl;
-  return 0;
+    char str[10 + 1] = "a b c d";
+
+    Solution solu;
+    solu.replaceSpace(str, 10);
+
+    cout <<str <<endl;
+
+    return 0;
 }
